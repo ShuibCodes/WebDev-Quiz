3c5dae08c4908c862fff6d49ab31a12f8cbdbbbf
ShuibCodes <71759511+ShuibCodes@users.noreply.github.com>
2020-09-25 21:28:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 // Selecting Elements
-const hint1 = document.querySelector(".hint1");
+const hint = document.querySelector(".hint");
 const hint2 = document.querySelector(".hint2");
 const hint3 = document.querySelector(".hint3");
 const hint4 = document.querySelector(".hint4");
 const form = document.querySelector('.quiz-form');
 const answers = ['B' , 'B' , 'B' , 'B'];
 const btn = document.querySelector(".submit");
+const answerTab = document.querySelector(".ansbtn");
+const answerTab2 = document.querySelector(".ansbtn2")
+const answerTab3 = document.querySelector(".ansbtn3");
+const answerTab4 = document.querySelector(".ansbtn4")
+const arrayAnswer = [answerTab,answerTab2,answerTab3,answerTab4];
+
+// answer cards
+const answerCard1 = document.querySelector(".answerCard1");
+const answerCard2 = document.querySelector(".answerCard2")
+
+// when page is loaded, automatically hide the answers
+
+window.addEventListener('DOMContentLoaded', (event) => {
+arrayAnswer.forEach(function(el){
+  el.style.display ="none";
+})
+});
+
 
 
 // Click "Need a Hint?" to see Hint:
 // Hint for Question 1:
-  seeHint1();
+  seeHint();
 // HInt for Question 2:
   seeHint2();
 // Hint for Question 3:
@@ -20,6 +38,13 @@ const btn = document.querySelector(".submit");
 
 
 // When submit is clicked-
+QuestionBank1();
+QuestionBank2()
+QuestionBank3()
+QuestionBank4();
+ScrollTop();
+hidePrompt();
+seeAnswers();
 
 btn.addEventListener("click", function(e){
     e.preventDefault();
@@ -27,7 +52,7 @@ btn.addEventListener("click", function(e){
   let score = 0;
   const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value] ;
 
-  // check the answers
+  // check the answers and add 25% for each correct answer
   userAnswers.forEach((answer, index) => {
     if (answer === answers[index]){
       score += 25;
@@ -52,20 +77,18 @@ btn.addEventListener("click", function(e){
     
 })
 
-// // creating a button to display hints 1
-
-function seeHint1(){
+// creating a button to display hints 1
+function seeHint(){
   const hintBtn = document.querySelector(".btnOne");
   hintBtn.addEventListener("click",function(){
-      if (hint1.style.display === "none") {
-          hint1.style.display = "block";
+      if (hint.style.display === "none") {
+          hint.style.display = "block";
         } else {
-          hint1.style.display = "none";
+          hint.style.display = "none";
         }
   });
-}  
+}
 
-// Creating button to display hint 2
 
 function seeHint2(){
   const hintBtn2 = document.querySelector(".btnTwo");
@@ -99,7 +122,165 @@ function seeHint4(){
 }  
 
 
+// function to hide answers and show when quiz is finished
+
+function seeAnswers(){
+  btn.addEventListener("click" , function(){
+    arrayAnswer.forEach(function(e){
+      e.style.display = "block";
+    })
 
 
+  })
+
+}
+// Creating btn that takes user to top of page for results after submit:
+
+function ScrollTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// function to hide " on with the questions" after results are shown:
+
+function hidePrompt(){
+  const prompt = document.querySelector(".prompt");
+  btn.addEventListener('click', function(){
+   prompt.style.display = "none"
+  })
+}
 
 
+// when user clicks answer to transiton to answer cards
+
+function QuestionBank1(){
+  // getting answer button and cards
+  const answerTab = document.querySelector(".ansbtn");
+  const questionCard1 = document.querySelector(".questionCard1");
+  const answerCard1 = document.querySelector(".answerCard1");
+
+// showing answer card
+
+    answerTab.addEventListener("click", function(){
+    if(questionCard1.style.display = "block"){
+      questionCard1.style.display = "none";
+      answerCard1.style.display = "block";
+  } else{
+    questionCard1.style.display = "block";
+    answerCard1.style.display = "none";
+  }
+
+   //getting easy and hard buttons
+   const Easybtn = document.querySelector(".Easybtn")
+  const Hardbtn = document.querySelector(".Hardbtn") 
+
+//  changing color of div once buttons are clicked
+
+  Easybtn.addEventListener("click", function(){
+    answerCard1.classList.toggle("Green")
+  })
+  Hardbtn.addEventListener("click", function(){
+    answerCard1.classList.toggle("Red");
+  })
+
+  })
+ 
+
+}
+function QuestionBank2(){
+  // getting answer button and cards
+  const answerTab2 = document.querySelector(".ansbtn2");
+  const questionCard2 = document.querySelector(".questionCard2");
+  const answerCard2 = document.querySelector(".answerCard2");
+// showing answer card 
+    answerTab2.addEventListener("click", function(){
+    if(questionCard2.style.display = "block"){
+      questionCard2.style.display = "none";
+      answerCard2.style.display = "block";
+  } else{
+    questionCard2.style.display = "block";
+    answerCard2.style.display = "none";
+  }
+      
+
+  })
+    // getting easy and hard buttons 
+  const Easybtn2 = document.querySelector(".Easybtn2")
+  const Hardbtn2 = document.querySelector(".Hardbtn2")
+
+  // changing color of div once buttons are clicked 
+
+  Easybtn2.addEventListener("click", function(){
+    answerCard2.classList.toggle("Green")
+    
+  })
+  Hardbtn2.addEventListener("click", function(){
+    answerCard2.classList.toggle("Red");
+  })
+}
+
+
+function QuestionBank3(){
+  // getting answer button and cards
+  const answerTab3 = document.querySelector(".ansbtn3");
+  const questionCard3 = document.querySelector(".questionCard3");
+  const answerCard3 = document.querySelector(".answerCard3");
+// showing answer card 
+    answerTab3.addEventListener("click", function(){
+    if(questionCard3.style.display = "block"){
+      questionCard3.style.display = "none";
+      answerCard3.style.display = "block";
+  } else{
+    questionCard3.style.display = "block";
+    answerCard3.style.display = "none";
+  }
+      
+
+  })
+    // getting easy and hard buttons 
+  const Easybtn3 = document.querySelector(".Easybtn3")
+  const Hardbtn3 = document.querySelector(".Hardbtn3")
+
+  // changing color of div once buttons are clicked 
+
+  Easybtn3.addEventListener("click", function(){
+    answerCard3.classList.toggle("Green")
+    
+  })
+  Hardbtn3.addEventListener("click", function(){
+    answerCard3.classList.toggle("Red");
+  })
+}
+
+function QuestionBank4(){
+  // getting answer button and cards
+  const answerTab4 = document.querySelector(".ansbtn4");
+  const questionCard4 = document.querySelector(".questionCard4");
+  const answerCard4 = document.querySelector(".answerCard4");
+// showing answer card 
+    answerTab4.addEventListener("click", function(){
+    if(questionCard4.style.display = "block"){
+      answerCard4.style.display = "block";
+      questionCard4.style.display = "none";
+
+  } else{
+    questionCard4.style.display = "block";
+    answerCard4.style.display = "none";
+  }
+      
+
+  })
+    // getting easy and hard buttons 
+  const Easybtn4 = document.querySelector(".Easybtn4")
+  const Hardbtn4 = document.querySelector(".Hardbtn4")
+
+  // changing color of div once buttons are clicked 
+
+  Easybtn4.addEventListener("click", function(){
+    answerCard4.classList.toggle("Green")
+    
+  })
+  Hardbtn4.addEventListener("click", function(){
+    answerCard4.classList.toggle("Red");
+  })
+}
