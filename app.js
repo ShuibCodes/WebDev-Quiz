39d89bae3051c14edec7e7743416ceb2b669d3ec
shuibcodes <abdullahshuib124@gmail.com>
2021-02-13 17:00:08 +0000

    layout

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const Hardbtn = document.querySelector(".Hardbtn")
  const questionCard2 = document.querySelector(".questionCard2");
  const questionCard3 = document.querySelector(".questionCard3");
  const questionCard4 = document.querySelector(".questionCard4");
+ const questionCard5 = document.querySelector(".questionCard5");
  const answerCard1 = document.querySelector(".answerCard1");
  const answerCard2 = document.querySelector(".answerCard2");
  const answerCard3 = document.querySelector(".answerCard3");
@@ -43,15 +44,17 @@ seeAnswerTab();
 seeAnswerCards()
 changeColors();
 
+
+
 function seeAnswerCards(){
  
 let allAnswers = [answerCard1,answerCard2,answerCard3,answerCard4, answerCard5]
-let allQuestions =[questionCard1,questionCard2,questionCard3,questionCard4]
+let allQuestions =[questionCard1,questionCard2,questionCard3,questionCard4,questionCard5]
     answerTab.addEventListener("click", function(){
       answerTab.style.display = "none"
      
       allAnswers.forEach(function(e){
-        e.style.display = "block";
+       e.style.display = "block"
         
       })
       allQuestions.forEach(function(e){
